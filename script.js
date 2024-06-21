c396b9d8b6766b7c9d25ae97f64eaf8b5014bcc7
Alex Chon <achon@uwaterloo.ca>
2024-06-21 18:08:50 +0900

    Initialize JS Variables Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const numberButtons = document.querySelectorAll('[data-number]');
+    const operationButtons = document.querySelectorAll('[data-operations]');
+    const equalsButton = document.querySelector('[data-equals]');
+    const deleteButton = document.querySelectorAll('[data-delete]');
+    const allClearButton = document.querySelectorAll('[data-all-clear]');
+    const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]');
+    const currentOperandTextElement = document.querySelectorAll('[data-current-operand]');
+});
+
