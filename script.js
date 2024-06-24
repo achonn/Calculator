dcba066797a0bd8fd0fbb273a5690ce5dae4cb1a
Alex Chon <achon@uwaterloo.ca>
2024-06-24 11:41:25 +0900

    Only Works with 2 Inputted Numbers

--- script.js
+++ script.js
@@ -22,22 +22,25 @@ let secondNumArray = [];
 function getNumber(number) {
     if (step === 0 || step === 1) {
         firstNumArray.push(number);
+        step = 1;
         currentOperand = Number(firstNumArray.join(''));
         currentOperandTextElement.innerText = currentOperand;
     } else if (step === 2) {
-        previousOperand = currentOperand;
-        currentOperand = '';
         secondNumArray.push(number);
         currentOperand = Number(secondNumArray.join(''));
-        previousOperandTextElement.innerText = previousOperand;
         currentOperandTextElement.innerText = currentOperand;
     }
 }
 
 
 function chooseOperation(op) {
-    step = 2;
-    operation = op;
+    if (step === 1) {
+        step = 2;
+        operation = op;
+        previousOperand = currentOperand;
+        previousOperandTextElement.innerText = `${previousOperand} ${operation}`;
+        currentOperand = '';
+    }
 }
 
 function clear() {  
@@ -47,12 +50,44 @@ function clear() {
     step = 0;
     firstNumArray = [];
     secondNumArray = [];
-    previousOperandTextElement.innerText = previousOperand;
-    currentOperandTextElement.innerText = currentOperand;
+    previousOperandTextElement.innerText = '';
+    currentOperandTextElement.innerText = '0';
 }
 
+
+
 function compute() {
-    console.log('Pressed Equals');
+    let result;
+    const firstNum = parseFloat(previousOperand);
+    const secondNum = parseFloat(currentOperand);
+
+    if (isNaN(firstNum) || isNaN(secondNum)) return;
+
+
+    switch (operation) {
+        case '+':
+            result = firstNum + secondNum;
+            break;
+        case '-':
+            result = firstNum - secondNum;
+            break;
+        case '*':
+            result = firstNum * secondNum;
+            break;
+        case '/':
+            result = firstNum / secondNum;
+            break;
+        default:
+            return;
+    }
+    currentOperandTextElement.innerText = result;
+    previousOperandTextElement.innerText = '';
+    currentOperand = result.toString();
+    previousOperand = '';
+    operation = undefined;
+    step = 0;
+    firstNumArray = [];
+    secondNumArray = [];
 }
 
 
