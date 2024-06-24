d0a752466a2b5183af681ee634624ec286a3eea8
Alex Chon <achon@uwaterloo.ca>
2024-06-24 09:43:05 +0900

    Function Inital Commit

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const numberButtons = document.querySelectorAll('[data-number]');
-    const operationButtons = document.querySelectorAll('[data-operations]');
-    const equalsButton = document.querySelector('[data-equals]');
-    const deleteButton = document.querySelectorAll('[data-delete]');
-    const allClearButton = document.querySelectorAll('[data-all-clear]');
-    const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]');
-    const currentOperandTextElement = document.querySelectorAll('[data-current-operand]');
-});
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operations]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelectorAll('[data-delete]');
+const allClearButton = document.querySelectorAll('[data-all-clear]');
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]');
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]');
 
+
+function clear() {
+
+};
+
+
+
+function delete() {
+
+};
+
+
+function appendNumber(number) {
+
+}
+
+
+function chooseOperation(operation) {
+
+}
+
+
+function compute() {
+
+}
+
+function updateDisplay() {
+
+}
